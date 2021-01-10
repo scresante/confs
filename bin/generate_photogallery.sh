23fbded410d14a2193ce70ac192653ab47ad4c0e
shawn cresante <shawn@cresante.com>
2021-01-10 01:48:18 -0500

    photogallery generator for website

---
+++ bin/generate_photogallery.sh
@@ -0,0 +1,62 @@
+#!/bin/zsh
+YEAR=2013
+
+#start=$(python -c "from datetime import datetime; print((datetime.now() - datetime($YEAR, 1, 1)).days)")
+#end=$(python -c "from datetime import datetime; print((datetime.now() - datetime($YEAR+1, 1, 1)).days)")
+#
+##copy files
+#for x in ../**/*(.md+${end}md-${start}Lm-50); do cp --reflink=always "$x" ./; done
+##resize big ones
+#for x in (#i)*jpg(-Lm+2); do convert "$x" -resize 50% "$x"; echo -n '.'; done
+#mkdir thumbs
+#mogrify -format gif -path thumbs -thumbnail 100x100 (#i)*(png|jpg|jpeg|gif)
+
+if [ ! -e "style.css" ]; then
+    echo ''' img {
+  border: 1px solid #ddd; /* Gray border */
+  border-radius: 4px;  /* Rounded border */
+  padding: 5px; /* Some padding */
+  width: 150px; /* Set a small width */
+}
+
+/* Add a hover effect (blue shadow) */
+img:hover {
+  box-shadow: 0 0 2px 1px rgba(0, 140, 186, 0.5);
+}
+
+.c { position: relative; }
+.x {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 11px;
+  background-color: coral;
+  border: 1px dotted black;
+}''' > style.css
+fi
+
+echo '''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta name="generator" content="zsh" />
+    <title>highly classified CIA photo dump ('''$YEAR''', colorized)</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body bgcolor="#333">''' > index.html
+for x in *(.); do
+    if [ ! -e thumbs/${x%.*}.gif ]; then
+        img='/generic.gif'
+    else
+        img="thumbs/${x%.*}.gif"
+    fi
+    echo """<span class=\"c\">
+    <a href=\"$x\">
+    <img src=\"$img\">
+    <span class=\"x\">$x</div>
+    </a>
+    </span>""" >> index.html
+done
+
+echo "</body>" >> index.html
